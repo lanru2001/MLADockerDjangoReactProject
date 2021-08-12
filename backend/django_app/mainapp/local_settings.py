e94be8f7092ce00a61c135222de30b5596e99544
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-08-11 19:56:26 -0500

    Create local_settings.py

---
+++ backend/django_app/mainapp/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'predictiondb',
+        'USER': 'postgres_user',
+        'PASSWORD': 'postgres_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
