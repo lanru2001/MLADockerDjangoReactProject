368441863ea1d3138b667bf2b57f35e43a90858e
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-08-11 20:04:36 -0500

    Update settings.py

--- backend/django_app/mainapp/settings.py
+++ backend/django_app/mainapp/settings.py
@@ -16,3 +16,16 @@ try:
     from .local_settings import * 
 except ImportError: 
     pass 
+
+# Application definition(Django RestAPI framework)
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'prediction',
+    'rest_framework',
+]
