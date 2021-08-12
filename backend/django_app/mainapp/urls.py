731d46e26fad03d75f9eefeb21de8dca484050a5
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-08-11 20:25:22 -0500

    Update urls.py

--- backend/django_app/mainapp/urls.py
+++ backend/django_app/mainapp/urls.py
@@ -16,3 +16,11 @@ urlpatterns = [
     path('api/auth/', include('users.urls')),
     path('api/', include('prediction.urls')),
 ]
+
+from django.urls import path, include
+
+from users.views import APILoginView, APILogoutView
+urlpatterns = [
+    path('login/', APILoginView.as_view(), name='api_login'),
+    path('logout/', APILogoutView.as_view(), name='api_logout'),
+]
