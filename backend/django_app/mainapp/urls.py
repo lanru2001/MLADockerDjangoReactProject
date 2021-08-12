9141750ea4e27306cb597b3bf307c70be41c6412
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-08-11 20:22:59 -0500

    Update urls.py

--- backend/django_app/mainapp/urls.py
+++ backend/django_app/mainapp/urls.py
@@ -6,3 +6,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('prediction.urls')),
 ]
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/auth/', include('users.urls')),
+    path('api/', include('prediction.urls')),
+]
