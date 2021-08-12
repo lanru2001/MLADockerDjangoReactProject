43ddfe6629491e184c7e96504fed73d0feca5982
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-08-11 20:30:58 -0500

    Update views.py

--- backend/django_app/prediction/views.py
+++ backend/django_app/prediction/views.py
@@ -28,14 +28,35 @@
 
 #We ensure that only the user who is logged in is able to log themselves out by authenticating with the token they received while logging in. 
 #This is what the authentication and permission classes in the APILogoutView dofrom rest_auth.views import (LoginView, LogoutView)
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+from prediction.apps import PredictionConfig
+import pandas as pd
+
 # Create your views here.
-class APILogoutView(LogoutView):
+# Class based view to predict based on IRIS model
+class IRIS_Model_Predict(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
-
-class APILoginView(LoginView):
-    pass    
+    
+    def post(self, request, format=None):
+        data = request.data
+        keys = []
+        values = []
+        for key in data:
+            keys.append(key)
+            values.append(data[key])
+        X = pd.Series(values).to_numpy().reshape(1, -1)
+        loaded_mlmodel = PredictionConfig.mlmodel 
+        y_pred = loaded_mlmodel.predict(X)
+        y_pred = pd.Series(y_pred)
+        target_map = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
+        y_pred = y_pred.map(target_map).to_numpy()
+        response_dict = {"Predicted Iris Species": y_pred[0]}
+        return Response(response_dict, status=200)
