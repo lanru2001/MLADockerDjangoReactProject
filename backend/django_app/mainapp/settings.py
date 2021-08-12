78f0318a79c624972d9f911267ef08791597a331
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-08-11 20:13:39 -0500

    Update settings.py

--- backend/django_app/mainapp/settings.py
+++ backend/django_app/mainapp/settings.py
@@ -29,3 +29,18 @@ INSTALLED_APPS = [
     'prediction',
     'rest_framework',
 ]
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'prediction',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+]
